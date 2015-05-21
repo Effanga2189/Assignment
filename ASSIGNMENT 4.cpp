5cdaa5a07ad64c817133856d0cff4b72e7e7f964
Kingsley2189 <kingsleyeffanga@yahoo.com>
2015-05-21 14:42:36 +0100

    Codes for Assignment #1
    The Repository contain source codes of the  Assignment #1 develop by dev
    c++

---
+++ ASSIGNMENT 4.cpp
@@ -0,0 +1,51 @@
+// NAME:EFFANGA,KINGSLEY OKON
+// REG NO:45652928HF
+// COURSE:CSC 181 ASSIGNMENT
+
+
+
+
+
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+}
